46830df9e5b2175c8c2d8ad51c634c8853fc773a
TKehlenbach <tkehlenbach@enlyze.com>
2025-08-04 14:47:33 +0200

    add case class

--- src/conversion_methods/event_log.py
+++ src/conversion_methods/event_log.py
@@ -14,3 +14,16 @@ class EventIdHandler:
     def reset(self):
         self.current = 0
         return
+
+class Case:
+
+    def __init__(self, name: str, attributes: dict | None) -> object:
+        # always: name (part/s, harmony, dynamic)
+        self.name = name
+        self.attributes = (
+            attributes  # sometimes: instrument, keys, clefs, tempos, meters
+        )
+        self.trace = []
+
+    def add_events(self, events: list[dict]) -> None:
+        self.trace.extend(events)
